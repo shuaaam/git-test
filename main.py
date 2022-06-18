104ea2374a8d292b8350db5dc6b381a00645b004
kravchenmd <kravchenmd@gmail.com>
2022-06-18 19:53:05 +0200

    Broken everything

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from utils.units import sub
 
-def print_hi(name):
-    res = sub(5, 3)
-    print(f'Hi, {name}: {res}')  # Press ⌘F8 to toggle the breakpoint.
+
+def print_hi(name, familyname):
+    res = f'Hi, {name} {familyname}'
+    print(res)  # Press ⌘F8 to toggle the breakpoint.
+
+
+def print_hi_2(name, familyname):
+    res = f'Hi, {name} {familyname}'
+    print(res)  # Press ⌘F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def main():
+    print_hi('John', 'Smith')
+    print_hi_2('John', 'Smith')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
