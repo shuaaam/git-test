064ed98558072d12429ab6a865571bf95ee35b8f
vlad-bb <96999713+vlad-bb@users.noreply.github.com>
2022-06-18 21:32:42 +0300

    new func

--- main.py
+++ main.py
@@ -1,30 +1,7 @@
-def star(n):
-    for i in range(n // 2 + 1):
-        for j in range(i + 1):
-            print('*', end='\r')
-        print()
-        print()
-def print_hi(name, familyname):
-    result = f'Hi, {name} {familyname}'
-    return result  # Press ⌘F8 to toggle the breakpoint.
-
-
-def print_hi_2(name, familyname):
-    res = f'Hi, {name} {familyname}'
-    print(res)  # Press ⌘F8 to toggle the breakpoint.
-
-
-def mappp(n):
-    for i in range(n // 2 + 1):
-        for j in range(i + 1):
-            print('*', end='')
-        print()
-    for k in range(n // 2, 0, -1):
-        for v in range(k):
-            print('*', end='')
-        print()
+def adding(a, b):
+    return a + b
 
 
 if __name__ == '__main__':
-    mappp(9)
-    print('\r')
+
+    print(adding(10, 5))
