5cac37b366ebba440b9d894e4d15fdc6fca738e8
vlad-bb <96999713+vlad-bb@users.noreply.github.com>
2022-06-18 20:54:31 +0300

    add new feature

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from utils.units import sub
-
-def print_hi(name):
-    res = sub(5, 3)
-    print(f'Hi, {name}: {res}')  # Press ⌘F8 to toggle the breakpoint.
+def star(n):
+    for i in range(n // 2 + 1):
+        for j in range(i + 1):
+            print('*', end='')
+        print()
+    for k in range(n // 2, 0, -1):
+        for v in range(k):
+            print('*', end='')
+        print()
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    star(9)
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
