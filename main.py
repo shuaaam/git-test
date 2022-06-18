07ef82be7654455b5de4f99225f8d51508cf735f
kravchenmd <kravchenmd@gmail.com>
2022-06-18 20:32:21 +0200

    1

--- main.py
+++ main.py
@@ -4,21 +4,17 @@ def star(n):
             print('*', end='\r')
         print()
         print()
-def print_hi(name, familyname):
+
+
+def print_hi(name, familyname, optional=0):
     result = f'Hi, {name} {familyname}'
-    return result  # Press ⌘F8 to toggle the breakpoint.
-
-
-def print_hi_2(name, familyname):
-    res = f'Hi, {name} {familyname}'
-    print(res)  # Press ⌘F8 to toggle the breakpoint.
+    return optional  # Press ⌘F8 to toggle the breakpoint.
 
 
 def mappp(n):
     for i in range(n // 2 + 1):
         for j in range(i + 1):
             print('*', end='')
-        print()
     for k in range(n // 2, 0, -1):
         for v in range(k):
             print('*', end='')
