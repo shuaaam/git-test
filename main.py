983a0de85776628e8499750c1e5e1c5d538e7e03
Oleksandra Melnyk <oleksandramelnyk.ibm@gmail.com>
2022-06-18 21:22:40 +0300

    delete main

--- main.py
+++ main.py
@@ -26,10 +26,6 @@ def print_hi_2(name, familyname):
 
 
 # Press the green button in the gutter to run the script.
-def main():
-    print_hi('John', 'Smith')
-    print_hi_2('John', 'Smith')
-
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
