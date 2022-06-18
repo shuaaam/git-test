76a5b3436a17d3c3d01e0f915e444853049769a6
Oleksandra Melnyk <oleksandramelnyk.ibm@gmail.com>
2022-06-18 20:25:41 +0300

    shelve added

---
+++ utils/addressbook.py
@@ -0,0 +1,17 @@
+from collections import UserDict
+import shelve
+
+class AddressBook(UserDict):
+    def __init__(self):
+        super().__init__()
+        self.n = None
+
+        filename = 'database/ab_data'
+        dt = self.data
+
+        with shelve.open(filename) as fn:
+                fn['dt'] = dt
+
+        with shelve.open(filename) as states:
+            for key in states:
+                print(f'{key}: {states[key]}')
