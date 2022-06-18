d16ce77846b8975debde58dc651540bd982ff370
Oleksandra Melnyk <oleksandramelnyk.ibm@gmail.com>
2022-06-18 19:58:56 +0300

    add op sub

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
+from utils.units import sub
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    res = sub(5, 3)
+    print(f'Hi, {name}: {res}')  # Press ⌘F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
